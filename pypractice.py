914b765625fca495f13454a211ab758d85d0e097
Anurag Singh <87132944+AnuragAS1@users.noreply.github.com>
2022-11-04 14:00:41 +0545

    Add files via upload

---
+++ pypractice.py
@@ -0,0 +1,35 @@
+#sentence = input("Enter a sentence : ")
+#print(sentence)
+#vowels=0
+#for i in sentence:
+#      if(i=='a' or i=='e' or i=='i' or i=='o' or i=='u' or i=='A' or i=='E' or i=='I' or i=='O' or i=='U'):
+#            vowels=vowels+1
+#print("Number of vowels are:")
+#print(vowels)
+#word_length = len(sentence)
+#consonant=word_length - vowels
+#print(consonant)
+
+
+
+
+#ch = input("Enter a character: ")
+#if ch.isdigit():
+#    print(ch, "try again ")
+    
+#if(ch=='A' or ch=='a' or ch=='E' or ch =='e' or ch=='I'
+# or ch=='i' or ch=='O' or ch=='o' or ch=='U' or ch=='u'):
+#    print(ch, "is a Vowel")
+#else:
+#    print(ch, "is a Consonant")
+
+
+list_a= [1, 3, 4, -2, 566]
+list_b= [3, 4, 5, 7, -333, 0]
+result = list_a+list_b
+my_set= set(result)
+my_new_list= list(my_set)
+my_new_list.sort()
+print('list in order: ', my_new_list)
+    
+
